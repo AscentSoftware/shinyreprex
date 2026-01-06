7537427ec9c44749279ca5e5f451e2a2b13945a4
Ashley Baldry <ashley.baldry@ascent.io>
2026-01-06 16:10:17 +0000

    Update reactive chunk tests

--- tests/testthat/test-chunk_reactive.R
+++ tests/testthat/test-chunk_reactive.R
@@ -1,4 +1,72 @@
-test_that("Able to extract reactive object from bindCache", {
+# repro_chunk ----
+test_that("Reactive code chunk is correctly extracted", {
+  test_server <- function(input, output, session) {
+    iris_filt <- reactive(iris[with(iris, Sepal.Width > input$min_width), ])
+
+    summary_tbl <- reactive({
+      aggregate(
+        Sepal.Width ~ Species,
+        data = iris_filt(),
+        FUN = get(input$summary_fn)
+      )
+    })
+  }
+
+  shiny::testServer(
+    test_server,
+    expr = {
+      session$setInputs(min_width = 3.5, summary_fn = "median")
+      repro_code <- repro_chunk(summary_tbl)
+
+      expect_s7_class(repro_code, Repro)
+      expect_identical(
+        repro_code@code,
+        list(str2lang("aggregate(Sepal.Width ~ Species, data = iris_filt, FUN = get(\"median\"))"))
+      )
+      expect_identical(
+        repro_code@prerequisites,
+        list(
+          iris_filt = list(
+            str2lang("iris_filt <- iris[with(iris, Sepal.Width > 3.5), ]"))
+        )
+      )
+    }
+  )
+})
+
+# repro reactives ----
+test_that("Able to extract reactive expression code from a standard reactive", {
+  test_server <- function(input, output, session) {
+    iris_filt <- reactive(iris[with(iris, Sepal.Width > input$min_width), ])
+
+    summary_tbl <- reactive({
+      aggregate(
+        Sepal.Width ~ Species,
+        data = iris_filt(),
+        FUN = get(input$summary_fn)
+      )
+    })
+  }
+
+  shiny::testServer(
+    test_server,
+    expr = {
+      session$setInputs(min_width = 3.5, summary_fn = "median")
+
+      repro_code <- repro(summary_tbl)
+      expect_identical(
+        repro_code,
+        paste(
+          "iris_filt <- iris[with(iris, Sepal.Width > 3.5), ]",
+          "aggregate(Sepal.Width ~ Species, data = iris_filt, FUN = get(\"median\"))",
+          sep = "\n"
+        )
+      )
+    }
+  )
+})
+
+test_that("Able to extract reactive expression code from bindCache", {
   test_server <- function(input, output, session) {
     iris_filt <- reactive(iris[with(iris, Sepal.Width > input$min_width), ]) |>
       bindCache(input$min_width)
@@ -31,7 +99,7 @@ test_that("Able to extract reactive object from bindCache", {
   )
 })
 
-test_that("Able to extract reactive object from bindEvent", {
+test_that("Able to extract reactive expression code from bindEvent", {
   test_server <- function(input, output, session) {
     iris_filt <- reactive(iris[with(iris, Sepal.Width > input$min_width), ]) |>
       bindEvent(input$min_width)
@@ -64,7 +132,7 @@ test_that("Able to extract reactive object from bindEvent", {
   )
 })
 
-test_that("Able to extract reactive object from bindEvent and bindCache", {
+test_that("Able to extract reactive expression code from bindEvent and bindCache", {
   test_server <- function(input, output, session) {
     iris_filt <- reactive(iris[with(iris, Sepal.Width > input$min_width), ]) |>
       bindCache(iris_filt(), input$summary_fn) |>
@@ -101,7 +169,7 @@ test_that("Able to extract reactive object from bindEvent and bindCache", {
 
 })
 
-test_that("Able to extract reactive object from eventReactive", {
+test_that("Able to extract reactive expression code from eventReactive", {
   test_server <- function(input, output, session) {
     iris_filt <- eventReactive(input$min_width, iris[with(iris, Sepal.Width > input$min_width), ])
 
