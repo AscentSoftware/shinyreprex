b4e2126f7c86e68c3a5641fa185603d2c44af432
Ashley Baldry <ashley.baldry@ascent.io>
2026-05-05 09:43:21 +0100

    Address lint issue

--- R/call_chunk_reactval.R
+++ R/call_chunk_reactval.R
@@ -2,7 +2,9 @@
 #' Extracting the contents of the `reactiveVal` in a human-readable way
 #'
 #' @noRd
-S7::method(repro_call_chunk, class_call_reactval_setter) <- function(x, repro_code = Repro(), env = rlang::caller_env()) {
+S7::method(repro_call_chunk, class_call_reactval_setter) <- function(x,
+                                                                     repro_code = Repro(),
+                                                                     env = rlang::caller_env()) {
   warning(
     "`", rlang::call_name(x), "()` is a reactiveVal setter and has been omitted from the ",
     "reproduced script - setting reactive values cannot be reproduced outside of Shiny",
