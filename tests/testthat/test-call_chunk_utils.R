3dd596a382c4e29d4172714acc898de158077580
Ashley Baldry <arbaldry91@gmail.com>
2026-09-17 15:22:37 +0100

    (fix): ensure anon functions don't get misclassed as reactive calls

--- tests/testthat/test-call_chunk_utils.R
+++ tests/testthat/test-call_chunk_utils.R
@@ -10,3 +10,24 @@ test_that("Able to extract package name from unnamespaced function call", {
 test_that("When extract package from function call, base R packages are ignored", {
   expect_null(get_pkg_name(str2lang("nzchar('foo')")))
 })
+
+test_that("Call checks return FALSE, not NA, when the call has no function name", {
+  anon_call <- str2lang('get("mean")(1:10)')
+
+  expect_false(is_reactive_call(anon_call))
+  expect_false(is_reactive_val_call(anon_call))
+  expect_false(is_reactive_val_setter_call(anon_call))
+})
+
+test_that("A reactive calling an unnamed function is reproduced rather than erroring", {
+  testthat::skip_if_not_installed("shiny")
+
+  shiny::reactiveConsole(TRUE)
+  on.exit(shiny::reactiveConsole(FALSE), add = TRUE)
+
+  named_fn <- shiny::reactive(get("mean")(1:10))
+  expect_identical(reprex_reactive(named_fn), 'get("mean")(1:10)')
+
+  anon_fn <- shiny::reactive((function(x) x + 1)(1))
+  expect_identical(reprex_reactive(anon_fn), "(function(x) x + 1)(1)")
+})
